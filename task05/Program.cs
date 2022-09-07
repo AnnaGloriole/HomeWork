69a7084c57e41cdd5ce5bd9fad02d8aeed534832
Anna <anna.gloriole@gmail.com>
2022-09-07 16:52:32 +0300

    comment

--- task05/Program.cs
+++ task05/Program.cs
@@ -1,8 +1,12 @@
-﻿
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
 Console.WriteLine("введите 3х значное число");
 string number = Console.ReadLine ();
 Console.WriteLine(number[1]);
-
 
 //Можно ли так?
 //int number = new Random().Next(99,1000);      попробовала ч.з рендом, но ругается что:
@@ -10,4 +14,3 @@ Console.WriteLine(number[1]);
 //пыталась эксперементировать, но ругается о невозможности void
 //string number1 = Console.ReadLine(Console.WriteLine(number));
 //Console.WriteLine(Console.WriteLine(number)[1]);
-
