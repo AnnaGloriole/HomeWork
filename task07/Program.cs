08a56e35ca5fc3259137d1f43f64218a3767452f
Anna <anna.gloriole@gmail.com>
2022-09-09 01:03:31 +0300

    hard week

--- task07/Program.cs
+++ task07/Program.cs
@@ -7,11 +7,11 @@ Console.WriteLine("введите день недели в виде цифры")
 int number = int.Parse(Console.ReadLine());
 if (number >= 1 && number <= 5)
 {
-Console.WriteLine("Рабочий день! К станку!");
+Console.WriteLine("Нет. Рабочий день! К станку!");
 }
 else if (number == 6 || number == 7)
 {
-Console.WriteLine("Ура выходной! Делаем домашку GB!");
+Console.WriteLine("Да. Ура выходной! Делаем домашку GB!");
 }
 else 
 {
