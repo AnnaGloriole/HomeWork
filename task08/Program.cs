e24ce02a98168c4aeced6f664f32d9a407952e5b
Anna <anna.gloriole@gmail.com>
2022-09-09 18:59:08 +0300

    Palindrome2

--- task08/Program.cs
+++ task08/Program.cs
@@ -3,9 +3,22 @@
 //12821 -> да
 //23432 -> да
 
+//Console.WriteLine("введите 5ти значное число");
+//string number = Console.ReadLine ();
+//if (number[0] == number[4] && number[1] == number[3])
+//{
+//    Console.WriteLine("Да");
+//}
+//else
+//{
+//    Console.WriteLine("Нет");
+//}
+
 Console.WriteLine("введите 5ти значное число");
-string number = Console.ReadLine ();
-if (number[0] == number[4] && number[1] == number[3])
+int number = Math.Abs(int.Parse(Console.ReadLine()));
+Console.WriteLine(number);
+string num2 = Convert.ToString(number);
+if (num2[0] == num2[4] && num2[1] == num2[3])
 {
     Console.WriteLine("Да");
 }
@@ -13,3 +26,4 @@ else
 {
     Console.WriteLine("Нет");
 }
+//else if ()
