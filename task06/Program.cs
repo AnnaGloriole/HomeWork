22c4ed97fe30a4460e3e16dc20a3077cfd2a9ebc
Anna <anna.gloriole@gmail.com>
2022-09-09 03:03:41 +0300

    numbers

---
+++ task06/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("введите целое число");
+//int number = int.Parse(Console.ReadLine());
+int num = Math.Abs(int.Parse(Console.ReadLine()));
+
+//Console.WriteLine(Math.Abs(int.Parse(Console.ReadLine())));
+//int num = int.Parse(Console.ReadLine());
+if (num < 100)
+{
+Console.WriteLine("Увы, нет 3й цифры");
+}
+else 
+{
+Console.WriteLine(num);
+string num2 = Console.ReadLine();
+Console.WriteLine(num2[2]);
+}
+ 
+
+
