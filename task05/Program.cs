8766e5787e338988ac5c17877db4aa4d07f3d013
Anna <anna.gloriole@gmail.com>
2022-09-09 16:46:57 +0300

    three-digit number

--- task05/Program.cs
+++ task05/Program.cs
@@ -4,13 +4,13 @@
 //782 -> 8
 //918 -> 1
 
-Console.WriteLine("введите 3х значное число");
-string number = Console.ReadLine ();
-Console.WriteLine(number[1]);
+//Вариант 1
+//Console.WriteLine("введите 3х значное число");
+//string number = Console.ReadLine ();
+//Console.WriteLine(number[1]);
 
-//Можно ли так?
-//int number = new Random().Next(99,1000);      попробовала ч.з рендом, но ругается что:
-//Console.WriteLine(number); - при попытке вывести [1] по индексу говорит о несоответствии int, string
-//пыталась эксперементировать, но ругается о невозможности void
-//string number1 = Console.ReadLine(Console.WriteLine(number));
-//Console.WriteLine(Console.WriteLine(number)[1]);
+//Вариант 2
+int number = new Random().Next(99, 1000);
+Console.WriteLine(number);
+string num2 = Convert.ToString(number);
+Console.WriteLine(num2[1]);
