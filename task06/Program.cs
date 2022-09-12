848b919ee861339b9f92c7e0e7d74a14b350b511
Anna <anna.gloriole@gmail.com>
2022-09-12 19:26:37 +0300

    very cute program

--- task06/Program.cs
+++ task06/Program.cs
@@ -3,38 +3,25 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
-Console.WriteLine("введите целое число");
-int num = Math.Abs(int.Parse(Console.ReadLine())); //модуль, если число отрицательное
-if (num < 100)
+try
 {
-    Console.WriteLine("Увы, нет 3й цифры");
+    Console.WriteLine("Input integer:");
+    int num = int.Parse(Console.ReadLine()!);
+    if (num < 0)
+    {
+        num = num * -1;
+    }
+    if (num < 100)
+    {
+        Console.WriteLine("three-digit number go for a walk");
+    }
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine(num % 10);
 }
-else
+catch
 {
-    //Console.WriteLine(num);
-    string num2 = Convert.ToString(num);
-    Console.WriteLine(num2[2]);
+    Console.WriteLine("Try again, stupid");
 }
-
-//Подскажите, пожалуйста, какое решение проще по операциям:
-// 1. Это решение - поиск 3го числа по индексу
-// 2. Математический медод - % 10 в цикле?
-// Можно ли использоать char[] и int[] для lго способа?
-
-// Хотелось сделать так, чтобы при вводе букв, была указана ошибка ввода, но программа выдает ошибку сама "Input string is not in correct format"
-// Как это можно обойти? Пример ниже
-//Console.WriteLine("введите целое число");
-//int num = Math.Abs(int.Parse(Console.ReadLine())); //модуль, если число отрицательное
-//if (num < 100)
-//{
-//Console.WriteLine("Увы, нет 3й цифры");
-//}
-//else if (num >= 100)
-//{
-//string num2 = Convert.ToString(num);
-//Console.WriteLine(num2[2]);
-//}
-//else
-//{
-//Console.WriteLine("Вы ввели не число, попытайтесь еще, мы верим в Вас");
-//}
