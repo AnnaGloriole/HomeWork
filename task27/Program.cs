3df8d8a6cd5294c16571a82306291c581d3c289e
Anna <anna.gloriole@gmail.com>
2022-10-03 23:36:22 +0300

    sum natural numbers between n & 1

---
+++ task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int n = EnterInt("Enter natural number: ");
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumRec(int n)
+{
+ if (n == 0) return 0;
+ else return n + SumRec(n - 1);
+}
+
+Console.WriteLine(SumRec(n)); 
